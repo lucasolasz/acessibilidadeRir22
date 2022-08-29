2e01a99f5e73df1e46523a47a28d1919c9d759fc
lucasolasz <olaszlucas072@gmail.com>
2022-08-29 01:34:33 -0300

    Inclusao a hospedagem

--- app/Config/configuracao.php
+++ app/Config/configuracao.php
@@ -7,10 +7,19 @@ define('DB_PASS', '');
 define('DB_NAME', 'acessibilidaderir');
 define('DB_PORT', '3306');
 
+//Parametros para o Banco
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'sfsyst94_id18241842');
+// define('DB_PASS', '1X5l5DnM!E');
+// define('DB_NAME', 'sfsyst94_setoracessibilidade');
+// define('DB_PORT', '3306');
+
 //dirname() retorna o nome do diretorio em que o arquivo está
 define('APP', dirname(dirname(__FILE__)));
 
 define('URL', '/acessibilidadeRir22');
+
+// define('URL', 'https://www.setoracessibilidade.com.br');
 
 define('APP_NOME', 'Acessibilidade Rock in Rio 2022');
 
