1f9d54435e637d7bc64354ab907a731b66d1e65b
lucasolasz <olaszlucas072@gmail.com>
2022-09-04 13:42:55 -0300

    Adiciona cadeiras de rodas

---
+++ BaseDados/014_adicao_cadeiras_04_set.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_cadeira_rodas (id_cadeira_rodas,num_cadeira_rodas) VALUES
+(21,"21"),
+(22,"22"),
+(23,"23"),
+(24,"24"),
+(25,"25"),
+(26,"26"),
+(27,"27"),
+(28,"28"),
+(29,"29"),
+(30,"30");
